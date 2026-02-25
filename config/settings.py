33cdc45b0c2acc5cddcb6555dee4a84a536b7f8a
Chrysanthos Pepes <pepesxrisanthos@gmail.com>
2026-02-25 12:12:14 +0200

    chore: project apps init and added to settings

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
+    'ingestion.apps.IngestionConfig',
+    'comparison.apps.ComparisonConfig',
 ]
 
 MIDDLEWARE = [
